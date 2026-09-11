ecc1d680f1faded2b870e3aae1fd9e62909d4b38
Floris-1096069 <1096069@hr.nl>
2026-09-11 12:10:22 +0200

    Chore, removed now unused references to db

--- config.py
+++ config.py
@@ -4,10 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    #database
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///imageboard.db")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
     #uploads
     UPLOAD_FOLDER = "static/uploads"
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024
